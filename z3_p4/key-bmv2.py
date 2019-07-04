d3e7ce6333cb44bcc75e1e3a7faf06035e4d207c
Unknown <fabian.ruffy@gmail.com>
2019-07-04 11:39:59 +0200

    Update repository.

--- z3_p4/key-bmv2.py
+++ z3_p4/key-bmv2.py
@@ -1,25 +1,28 @@
-# (declare-const key (_ BitVec 32))
-# (declare-datatypes () ((H (mk-h (a (_ BitVec 32)) (b (_ BitVec 32)) ))))
-# (declare-const h H)
-# (define-fun pass_0 ((h H)) (H)
-#     (ite (= (bvadd (a h) (a h)) key) (mk-h (a h) (a h)) (mk-h (a h) (b h)) )
-# )
-# (define-const key_0 (_ BitVec 32) (bvadd (a h) (a h)))
-# (define-fun pass_1 ((h H)) (H)
-#     (ite (= key_0 key)  (mk-h (a h) (a h)) (mk-h (a h) (b h)) )
-# )
-# (assert (not ( = (pass_0 h) (pass_1 h))))
-# (check-sat)
-
-
 from z3 import *
 
-# declaration of the global header type that is to be parsed
+
+''' HEADERS '''
+# The input headers of the control pipeline
 hdr = Datatype('hdr')
 hdr.declare('mk_hdr', ('a', BitVecSort(32)), ('b', BitVecSort(32)))
 hdr = hdr.create()
 
-# the final output in a single datatype
+
+''' TABLES '''
+''' The table constant we are matching with.
+ Actually this should be a match action tuple that picks the next action
+ How to implement that? Some form of array?
+ Right now, we have a hacky version of integer values which mimic an enum.
+ Each integer value corresponds to a specific action PER table. The number of
+ available integer values is constrained. '''
+ma_c_t = Datatype('ma_c_t')
+ma_c_t.declare('mk_ma_c_t', ('key_0', BitVecSort(32)), ('action', IntSort()))
+ma_c_t = ma_c_t.create()
+
+
+''' OUTPUT '''
+
+# the final output of the control pipeline in a single datatype
 p4_output = Datatype('p4_output')
 p4_output.declare('mk_output',
                   ('hdr', hdr),
@@ -27,104 +30,130 @@ p4_output.declare('mk_output',
 p4_output = p4_output.create()
 
 
-# the table constant we are matching with
-# actually this should be a match action tuple that picks the next action
-# how to implement that? Some form of array?
-# actually, each table function should emit the header and egress spec as
-# they can potentially modify it
-ma_c_t = Datatype('ma_c_t')
-ma_c_t.declare('mk_ma_c_t', ('key_0', BitVecSort(32)), ('action', IntSort()))
-ma_c_t = ma_c_t.create()
+''' INPUT VARIABLES AND MATCH-ACTION ENTRIES'''
 
-
-# define the header and match-action entries
+# Initialize the header and match-action constraints
+# These are our inputs
+# Think of it as the header inputs after they have been parsed
 h = Const('h', hdr)
+# The possible table entries
 c_t_m = Const('c_t_m', ma_c_t)
 
 
 def control_ingress_0(s):
-    # This is the initial version of the program
-    ret = Const('ret', p4_output)
+    ''' This is the initial version of the program. '''
 
-    # the assignments that happen during the execution of the pipeline
-    constraints = []
+    # The output header, one variable per modification
+    ret_0 = p4_output.mk_output(h, BitVecVal(0, 9))
 
     def c_a_0():
         # This action creates a new header type where b is set to a
-        return (hdr.b(p4_output.hdr(ret)) == hdr.a(h))
+        return hdr.b(p4_output.hdr(ret_0)) == hdr.a(h)
 
     def NoAction_0():
+        ''' This is an action '''
         # NoAction just returns the current header as is
         return Var(True, BoolSort())
 
     def c_t():
+        ''' This is a table '''
         # reduce the range of action outputs to the total number of actions
+        # in this case we only have 2 actions
         s.add(0 < ma_c_t.action(c_t_m), ma_c_t.action(c_t_m) < 2)
 
         def default():
-            # The default action
+            ''' The default action '''
             # It is set to NoAction in this case
             return NoAction_0()
 
         def select_action():
+            ''' This is a special macro to define action selection. We treat
+            selection as a chain of implications. If we match, then the clause
+            returned by the action must be valid.
+            This might become really ugly with many actions.'''
             return And(Implies(ma_c_t.action(c_t_m) == 1,
                                c_a_0()),
                        Implies(ma_c_t.action(c_t_m) == 2,
                                NoAction_0()),
                        )
+        # The key of the table. In this case it is a single value
+        c_t_key_0 = hdr.a(h) + hdr.a(h)
         # This is a table match where we look up the provided key
-        key_0 = hdr.a(h) + hdr.a(h)
-        return If(key_0 == ma_c_t.key_0(c_t_m),
+        # If we match select the associated action, else use the default action
+        return If(c_t_key_0 == ma_c_t.key_0(c_t_m),
                   select_action(), default())
-    # begin apply
-    constraints.append(c_t())
-    constraints.append(p4_output.egress_spec(ret) == BitVecVal(0, 9))
-    return And(constraints)
+
+    def apply():
+        # The list of assignments during the execution of the pipeline
+        constraints = []
+
+        # begin the apply pipeline
+        constraints.append(c_t())
+        constraints.append(p4_output.egress_spec(ret_0) == BitVecVal(0, 9))
+        return constraints
+    # return the apply function as sequence of logic clauses
+    return And(apply())
 
 
 def control_ingress_1(s):
-    # This is the emitted program after pass
-    ret = Const('ret', p4_output)
-    # the assignments that happen during the execution of the pipeline
-    constraints = []
+    ''' This is the initial version of the program. '''
 
+    # The output header, one variable per modification
+    ret_0 = p4_output.mk_output(h, BitVecVal(0, 9))
+
+    # the key is defined in the control function
     key_0 = BitVec('key_0', 32)
 
     def c_a_0():
         # This action creates a new header type where b is set to a
-        return (hdr.b(p4_output.hdr(ret)) == hdr.a(h))
+        return hdr.b(p4_output.hdr(ret_0)) == hdr.a(h)
 
     def NoAction_0():
+        ''' This is an action '''
         # NoAction just returns the current header as is
         return Var(True, BoolSort())
 
     def c_t():
-
+        ''' This is a table '''
         # reduce the range of action outputs to the total number of actions
+        # in this case we only have 2 actions
         s.add(0 < ma_c_t.action(c_t_m), ma_c_t.action(c_t_m) < 2)
 
         def default():
-            # The default action
+            ''' The default action '''
             # It is set to NoAction in this case
             return NoAction_0()
 
         def select_action():
+            ''' This is a special macro to define action selection. We treat
+            selection as a chain of implications. If we match, then the clause
+            returned by the action must be valid.
+            This might become really ugly with many actions.'''
             return And(Implies(ma_c_t.action(c_t_m) == 1,
                                c_a_0()),
                        Implies(ma_c_t.action(c_t_m) == 2,
                                NoAction_0()),
                        )
-        # This is a table match where we look up the provided key
-        nonlocal key_0
+        # The key of the table. In this case it is a single value
+        nonlocal key_0  # we refer to a variable in the outer scope
         c_t_key_0 = key_0
+        # This is a table match where we look up the provided key
+        # If we match select the associated action, else use the default action
         return If(c_t_key_0 == ma_c_t.key_0(c_t_m),
                   select_action(), default())
 
-    # begin apply
-    key_0 = hdr.a(h) + hdr.a(h)
-    constraints.append(c_t())
-    constraints.append(p4_output.egress_spec(ret) == BitVecVal(0, 9))
-    return And(constraints)
+    def apply():
+        # The list of assignments during the execution of the pipeline
+        constraints = []
+
+        nonlocal key_0  # we refer to a variable in the outer scope
+        key_0 = hdr.a(h) + hdr.a(h)
+        # begin the apply pipeline
+        constraints.append(c_t())
+        constraints.append(p4_output.egress_spec(ret_0) == BitVecVal(0, 9))
+        return constraints
+    # return the apply function as sequence of logic clauses
+    return And(apply())
 
 
 def z3_check():
