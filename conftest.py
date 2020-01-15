2d8d5d2a56612e132b432118c23684615ab30329
fruffy <fruffy@nyu.edu>
2020-01-15 12:21:51 -0500

    Update test infrastructure, consider all P4_16 tests.

--- conftest.py
+++ conftest.py
@@ -16,3 +16,31 @@ def pytest_runtest_makereport(item):
     outcome = yield
     report = outcome.get_result()
     report.custom_err = getattr(item, 'custom_err', 0)
+
+
+# def pytest_collection_modifyitems(config, items):
+#     keywordexpr = config.option.keyword
+#     markexpr = config.option.markexpr
+#     if keywordexpr or markexpr:
+#         return  # let pytest handle this
+
+#     skip_p4xfail = pytest.mark.skip(reason='xfail not selected')
+#     for item in items:
+#         if "p4xfail" in item.keywords:
+#             item.add_marker(skip_p4xfail)
+
+
+def pytest_collection_modifyitems(items, config):
+    # add `always_run` marker to all unmarked items
+    # for item in items:
+    #     if not any(item.iter_markers()):
+    #         item.add_marker("always_run")
+    # Ensure the `always_run` marker is always selected for
+    markexpr = config.getoption("markexpr", "False")
+    keyword = config.getoption("keyword", "False")
+    if keyword:
+        config.option.keyword = keyword
+    elif markexpr:
+        config.option.markexpr = markexpr
+    else:
+        config.option.markexpr = f"always_run"
--- test.py
+++ test.py
@@ -30,6 +30,32 @@ P4C_BIN_1863 = FILE_DIR.joinpath("p4c_bins/issue1863")
 
 # ***** working tests *****
 bmv2_tests = [
+    "checksum1-bmv2.p4",
+    "checksum2-bmv2.p4",
+    "checksum3-bmv2.p4",
+    "fabric_20190420/fabric.p4",
+    "header-stack-ops-bmv2.p4",
+    "hit-expr-bmv2.p4",
+    "issue1127-bmv2.p4",
+    "issue1566-bmv2.p4",
+    "issue1566.p4",
+    "issue1653-complex-bmv2.p4",
+    "issue1765-bmv2.p4",
+    "issue1814-1-bmv2.p4",
+    "issue1814-bmv2.p4",
+    "issue1937-1-bmv2.p4",
+    "issue1937-3-bmv2.p4",
+    "issue1989-bmv2.p4",
+    "issue486-bmv2.p4",
+    "issue496.p4",
+    "issue949.p4",
+    "issue561-2-bmv2.p4",
+    "issue561-7-bmv2.p4",
+    "junk-prop-bmv2.p4",
+    "table-entries-range-bmv2.p4",
+    "table-entries-ser-enum-bmv2.p4",
+    "v1model-p4runtime-most-types1.p4",
+    "v1model-special-ops-bmv2.p4",
     "action_selector_shared-bmv2.p4",
     "action_selector_unused-bmv2.p4",
     "action_profile-bmv2.p4",
@@ -215,6 +241,11 @@ bmv2_tests = [
     # "flowlet_switching-bmv2.p4",
 ]
 
+bmv2_tests = []
+all_tests = list(P4_DIR.glob("*.p4"))
+for test in all_tests:
+    bmv2_tests.append(str(test.name))
+
 # ***** violation tests *****
 violation_tests = [
     "basic_routing_stripped",
@@ -253,7 +284,7 @@ xfails = [
     "checksum1-bmv2.p4",
     "checksum2-bmv2.p4",
     "checksum3-bmv2.p4",
-    "fabric_20190420/fabric.p4",
+    "fabric.p4",
     "header-stack-ops-bmv2.p4",
     "hit-expr-bmv2.p4",
     "issue1127-bmv2.p4",
@@ -276,12 +307,153 @@ xfails = [
     "table-entries-ser-enum-bmv2.p4",
     "v1model-p4runtime-most-types1.p4",
     "v1model-special-ops-bmv2.p4",
+    "arch1.p4",
+    "arch2.p4",
+    "arch3.p4",
+    "array_field.p4",
+    "array_field1.p4",
+    "cases.p4",
+    "cast_noop.p4",
+    "complex.p4",
+    "complex1.p4",
+    "complex10.p4",
+    "complex2.p4",
+    "concat.p4",
+    "control-as-param.p4",
+    "default-package-argument.p4",
+    "default-switch.p4",
+    "direct-action.p4",
+    "direct-action1.p4",
+    "emptyTuple.p4",
+    "enum-bmv2.p4",
+    "bool_cast.p4",
+    "constant_folding.p4",
+    "factory1.p4",
+    "functors9.p4",
+    "issue1544-2.p4",
+    "exit4.p4",
+    "extern3.p4",
+    "factory2.p4",
+    "function.p4",
+    "functors6.p4",
+    "functors7.p4",
+    "functors8.p4",
+    "generic.p4",
+    "generic1.p4",
+    "hit_ebpf.p4",
+    "inline-control.p4",
+    "inline-control1.p4",
+    "inline-switch.p4",
+    "interface.p4",
+    "issue1172.p4",
+    "issue1208-1.p4",
+    "issue1304.p4",
+    "issue1334.p4",
+    "issue1386.p4",
+    "issue1540.p4",
+    "issue1638.p4",
+    "issue1717.p4",
+    "issue1806.p4",
+    "issue1830.p4",
+    "issue2036-3.p4",
+    "issue2044-bmv2.p4",
+    "issue304-1.p4",
+    "issue304.p4",
+    "issue313_1.p4",
+    "issue313_2.p4",
+    "issue313_3.p4",
+    "issue314.p4",
+    "issue344.p4",
+    "issue396.p4",
+    "issue407-2.p4",
+    "issue407-3.p4",
+    "issue754.p4",
+    "issue803-2.p4",
+    "issue803-3.p4",
+    "issue871.p4",
+    "issue933.p4",
+    "issue982.p4",
+    "key-issue-1020_ebpf.p4",
+    "large.p4",
+    "list-compare.p4",
+    "lj_example.p4",
+    "module.p4",
+    "named-arg1.p4",
+    "nested-tuple.p4",
+    "p4rt_digest_complex.p4",
+    "package.p4",
+    "parser-conditional.p4",
+    "psa-action-profile1.p4",
+    "psa-action-profile2.p4",
+    "psa-action-profile3.p4",
+    "psa-action-profile4.p4",
+    "psa-action-selector1.p4",
+    "psa-action-selector3.p4",
+    "psa-basic-counter-bmv2.p4",
+    "psa-counter1.p4",
+    "psa-counter2.p4",
+    "psa-counter3.p4",
+    "psa-counter4.p4",
+    "psa-counter6.p4",
+    "psa-drop-all-bmv2.p4",
+    "psa-drop-all-corrected-bmv2.p4",
+    "psa-example-counters-bmv2.p4",
+    "psa-example-digest-bmv2.p4",
+    "psa-example-parser-checksum.p4",
+    "psa-fwd-bmv2.p4",
+    "psa-hash.p4",
+    "psa-meter1.p4",
+    "psa-meter3.p4",
+    "psa-meter4.p4",
+    "psa-meter5.p4",
+    "psa-meter6.p4",
+    "psa-multicast-basic-bmv2.p4",
+    "psa-multicast-basic-corrected-bmv2.p4",
+    "psa-portid-using-newtype2.p4",
+    "psa-random.p4",
+    "psa-recirculate-no-meta-bmv2.p4",
+    "psa-register1.p4",
+    "psa-register2.p4",
+    "psa-register3.p4",
+    "psa-resubmit-bmv2.p4",
+    "psa-test.p4",
+    "psa-top-level-assignments-bmv2.p4",
+    "psa-unicast-or-drop-bmv2.p4",
+    "psa-unicast-or-drop-corrected-bmv2.p4",
+    "rcp.p4",
+    "register-serenum.p4",
+    "serenum.p4",
+    "side_effects.p4",
+    "simplify.p4",
+    "spec-ex12.p4",
+    "spec-ex15.p4",
+    "spec-ex17.p4",
+    "spec-ex27.p4",
+    "stack.p4",
+    "stack2.p4",
+    "string.p4",
+    "struct_init-1.p4",
+    "logging.p4",
+    "nested-tuple1.p4",
+    "pipe.p4",
+    "psa-action-selector2.p4",
+    "psa-example-register2-bmv2.p4",
+    "rcp1.p4",
+    "table-key-serenum.p4",
+    "tuple.p4",
+    "tuple0.p4",
+    "tuple1.p4",
+    "tuple2.p4",
+    "twoPipe.p4",
+    "two_ebpf.p4",
+    "unreachable-accept.p4",
+    "v1model-digest-containing-ser-enum.p4",
+    "vss-example.p4",
 ]
 
-# bmv2_tests = []
-# all_tests = list(P4_DIR.glob("*.p4"))
-# for test in all_tests:
-#     bmv2_tests.append(str(test.name))
+slow_tests = [
+    "flowlet_switching-bmv2",
+]
 
 
 def prep_test(p4_name, p4_dir=P4_DIR):
@@ -293,9 +465,15 @@ def prep_test(p4_name, p4_dir=P4_DIR):
 
 
 def run_z3p4_test(p4_file, target_dir):
+    if p4_file.name in slow_tests:
+        pytest.skip(f"Skipping slow test {p4_file}.")
+        return util.EXIT_SKIPPED
+
     result = p4c_check.validate_translation(p4_file, target_dir, P4C_BIN)
     if result == util.EXIT_SKIPPED:
         pytest.skip(f"Skipping file {p4_file}.")
+    if p4_file.name in xfails:
+        pytest.xfail(f"Expected {p4_file} to fail.")
     return result
 
 
@@ -313,6 +491,7 @@ def run_violation_test(test_folder):
     return util.EXIT_SUCCESS
 
 
+@pytest.mark.always_run
 @pytest.mark.parametrize("test_name", bmv2_tests)
 def test_bmv2(request, test_name):
     p4_file, target_dir = prep_test(test_name)
@@ -327,6 +506,20 @@ def test_skipped(request, test_name):
     assert request.node.custom_err == util.EXIT_SKIPPED
 
 
+@pytest.mark.always_run
+@pytest.mark.parametrize("test_name", false_friends)
+def test_friends(request, test_name):
+    p4_file, target_dir = prep_test(test_name, FALSE_FRIENDS_DIR)
+    request.node.custom_err = run_z3p4_test(p4_file, target_dir)
+    assert request.node.custom_err == util.EXIT_SUCCESS
+
+
+@pytest.mark.always_run
+@pytest.mark.parametrize("test_folder", violation_tests)
+def test_violation(test_folder):
+    assert run_violation_test(test_folder) == util.EXIT_SUCCESS
+
+
 @pytest.mark.xfail
 @pytest.mark.parametrize("test_name", xfails)
 def test_xfails(request, test_name):
@@ -335,18 +528,7 @@ def test_xfails(request, test_name):
     assert request.node.custom_err == util.EXIT_SUCCESS
 
 
-@pytest.mark.parametrize("test_name", false_friends)
-def test_friends(request, test_name):
-    p4_file, target_dir = prep_test(test_name, FALSE_FRIENDS_DIR)
-    request.node.custom_err = run_z3p4_test(p4_file, target_dir)
-    assert request.node.custom_err == util.EXIT_SUCCESS
-
-
-@pytest.mark.parametrize("test_folder", violation_tests)
-def test_violation(test_folder):
-    assert run_violation_test(test_folder) == util.EXIT_SUCCESS
-
-
+@pytest.mark.always_run
 @pytest.mark.skip(reason="binary currently broken")
 def test_issue1863_broken():
     # ***** actual custom violation *****
