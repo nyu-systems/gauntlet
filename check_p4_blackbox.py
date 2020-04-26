d2b7f3f2b060f5cd94259a9e35fd8e9b27f4b1f9
fruffy <fruffy@nyu.edu>
2020-04-26 10:40:32 +0200

    Fix blackbox, update tracked bugs.

--- check_p4_blackbox.py
+++ check_p4_blackbox.py
@@ -106,7 +106,7 @@ def insert_spaces(text, dist):
 
 
 def get_stf_str(z3_model, z3_const, dont_care_map):
-    z3_input_header = z3_model[z3.Const("ingress_0", z3_const.sort())]
+    z3_input_header = z3_model[z3.Const("ig_0", z3_const.sort())]
     log.debug("Input header: %s", z3_input_header)
     input_values = fill_values(z3_input_header)
     input_pkt_str = "".join(convert_to_stf(input_values, "Headers"))
@@ -331,7 +331,7 @@ def get_dont_care_map(z3_input):
                 # both of these strings are special
                 # ingress means it is a variable we have control over
                 # invalid means that there is no byte output
-                if str(val) not in ("ingress_0", "invalid"):
+                if str(val) not in ("ig_0", "invalid"):
                     dont_care_vals.add(str_val)
             return assemble_dont_care_map(child, dont_care_vals)
         else:
@@ -352,7 +352,7 @@ def dissect_conds(conditions):
         has_table_action = False
         has_undefined_var = False
         for cond_var in z3.z3util.get_vars(cond):
-            if "ingress_0" in str(cond_var):
+            if "ig_0" in str(cond_var):
                 has_member = True
             elif "table_key" in str(cond_var):
                 has_table_key = True
@@ -368,6 +368,7 @@ def dissect_conds(conditions):
         else:
             fixed_conds.append(cond)
     for var in undefined_vars:
+        # fixme does not handle undefined data types
         undefined_conds.append(var == 0)
     return controllable_conds, fixed_conds, undefined_conds
 
