72f4eb68da4fe86a07556bce4e06c0e8bc221b98
fruffy <fruffy@nyu.edu>
2020-09-18 19:28:40 +0200

    Hotfix.

--- test.py
+++ test.py
@@ -94,6 +94,7 @@ xfails = [
     "complex2.p4",  # runtime index, now idea how to resolve this madness
     "issue1334.p4",  # overloading, this test should normally not be skipped
     "shadow-after-use.p4",  # very specific shadowing
+    "tuple3.p4",  # unsupported
 ]
 
 
