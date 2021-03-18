b4efe2fc8adc8b4316ce2d6f09a483786ceedb49
fruffy <fruffy@nyu.edu>
2021-03-18 12:06:34 -0400

    Proper xfail names.

--- test.py
+++ test.py
@@ -96,8 +96,8 @@ xfails = [
     "complex2.p4",  # runtime index, now idea how to resolve this madness
     "issue1334.p4",  # overloading, this test should normally not be skipped
     "shadow-after-use.p4",  # very specific shadowing
-    "tuple3",  # not implemented yet
-    "tuple4",  # not implemented yet
+    "tuple3.p4",  # not implemented yet
+    "tuple4.p4",  # not implemented yet
 ]
 
 
