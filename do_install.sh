a69e7c401cc73cef12995219d3df42a8e64d165d
fruffy <fruffy@nyu.edu>
2021-05-01 13:42:46 +0200

    Update install.

--- do_install.sh
+++ do_install.sh
@@ -114,15 +114,14 @@ cd ${SRC_DIR}
 fi
 
 
-# grab the toz3_v2 extension for the p4 compiler
+# grab the toz3 extension for the p4 compiler
 mkdir -p ${MODULE_DIR}/p4c/extensions
 # only install bludgeon if we are not running in CI
 if [ "$RUN_CI" == "ON" ]; then
     echo "Also linking extra modules..."
     ln -sf ${MODULE_DIR}/bludgeon ${MODULE_DIR}/p4c/extensions/bludgeon
-    ln -sf ${MODULE_DIR}/pruner ${MODULE_DIR}/p4c/extensions/pruner
 fi
-ln -sf ${MODULE_DIR}/toz3_v2 ${MODULE_DIR}/p4c/extensions/toz3_v2
+ln -sf ${MODULE_DIR}/toz3 ${MODULE_DIR}/p4c/extensions/toz3
 
 # build the p4 compiler
 echo "Building P4C..."
